28ac9ca5fb7932e2edcc1b6649616be000a9f47c
Mihir Sharma <mihir.sharma@unc.edu>
2023-09-30 12:46:01 -0400

    change collumn

--- fileOpenerHeatMap.py
+++ fileOpenerHeatMap.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 main_file_name, cities_file_name = "NautralSciences_Dataset.xlsx", "uscities.xlsx"
 main_sheet, cities_sheet = "Fires", "Sheet1"
-df = pd.read_excel(io=main_file_name, sheet_name=main_sheet, usecols="Y, AD, AE, AF",nrows = 50)
+df = pd.read_excel(io=main_file_name, sheet_name=main_sheet, usecols="Y, AC, AE, AF",nrows = 50)
 
 
--- heatmap.py
+++ heatmap.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly_express as px
 
+import fileOpenerSHORTER
 
-dataFrame = fileOpener.df
-
-dataFrame.drop(df.index[df.index > 10], inplace=True)
+dataFrame = fileOpenerSHORTER.df
 
 print(dataFrame)
