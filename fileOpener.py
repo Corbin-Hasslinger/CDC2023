db6d2d7fc6f2fcd9ab0d5135e7c87104232c2a03
Corbin-Hasslinger <corbin.hasslinger@gmail.com>
2023-09-30 11:57:11 -0400

    Open Excel file

--- fileOpener.py
+++ fileOpener.py
@@ -1,2 +1,6 @@
-print("Hello World")
-#Test Commit
+import pandas as pd
+
+file_name = "NautralSciences_Dataset.xlsx"
+sheet = "Fires"
+df = pd.read_excel(io=file_name, sheet_name=sheet)
+print(df.head(5))
