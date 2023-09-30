fd8a0adc5c1a3d53762dcb7276525878bdb31266
Corbin-Hasslinger <corbin.hasslinger@gmail.com>
2023-09-30 17:13:14 -0400

    Updated Analysis of total fires and classes

--- fileOpener.py
+++ fileOpener.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import joblib
 
-main_file_name, cities_file_name = "NautralSciences_Dataset.xlsx", "uscities.xlsx"
-main_sheet, cities_sheet = "Fires", "Sheet1"
-mf = pd.read_excel(io=main_file_name, sheet_name=main_sheet, usecols="Y, AC, AD, AE, AF")
+main_file_name, cities_file_name = "NautralSciences_Dataset.xlsx"
+main_sheet = "Fires"
+mf = pd.read_excel(io=main_file_name, sheet_name=main_sheet, usecols="Y, AC, AD, AE, AF, AI")
 with open('dataset.pkl', 'wb') as file:
     joblib.dump(mf, file)
-# cf = pd.read_excel(io = cities_file_name, sheet_name=cities_file_name, usecols="A, ")
-print(mf.head(5))
 
