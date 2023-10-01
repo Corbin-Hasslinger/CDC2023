83af9cd2fa63e83bd41d6f933dcc31439034ffcb
Mihir Sharma <mihir.sharma@unc.edu>
2023-09-30 20:25:39 -0400

    Update fileOpener.py

--- fileOpener.py
+++ fileOpener.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import joblib
 
-main_file_name, cities_file_name = "NautralSciences_Dataset.xlsx"
+main_file_name = "NautralSciences_Dataset.xlsx"
 main_sheet = "Fires"
 mf = pd.read_excel(io=main_file_name, sheet_name=main_sheet, usecols="U, T, V, Y, AC, AD, AE, AF")
 with open('dataset.pkl', 'wb') as file:
