17de95368350fc51a4fa8a482a6ab0711a440480
Corbin-Hasslinger <corbin.hasslinger@gmail.com>
2023-09-30 20:22:04 -0400

    Top G Counts

--- heatmap.py
+++ heatmap.py
@@ -3,9 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly_express as px
+import joblib
 
+with open('dataset.pkl', 'rb') as file:
+    df = joblib.load(file)
 
-dataFrame = fileOpener.df
+dataFrame = df
 
 dataFrame.drop(df.index[df.index > 10], inplace=True)
 
